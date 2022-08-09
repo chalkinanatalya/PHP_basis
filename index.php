08047ae83439520b67587295fb28f51bfe9c01ba
Natalya <m_d.r.lecter@protonmail.com>
2022-08-10 00:09:39 +0400

    cecles, conditions

--- index.php
+++ index.php
@@ -1,26 +1,77 @@
 <?php 
 
 //Task 1
-$name = readLine("Как тебя зовут?");
-$age = (int)readLine("Сколько тебе лет?");
 
-echo "Вас зовут $name, вам $age лет";
+$question = readLine("Who is the original creator of PHP language? 1.Rasmus Lerdorf 2.James Gosling 3.Brendan Eich ");
+$flag = false;
+
+ do {
+    switch($question) {
+        case $question === "1":
+            echo "Great! $question is correct!";
+            $flag = true;
+            break;
+         case $question === "2":
+            echo "$question is incorrect! He was Java creator.";
+            $flag = true;
+            break;
+        case $question === "3":
+            echo "$question is incorrect! He was a JavaScript creator.";
+            $flag = true;
+            break;
+        default:
+            $question = readLine("Who is the original creator of PHP language? 1.Rasmus Lerdorf 2.James Gosling 3.Brendan Eich");
+    }
+} while (!$flag);
+
 
 //Task 2
-$total_amount = 3;
-$task1 = readLine("Какая задача стоит перед вами сегодня?");
-$time1 = (int)readLine("Сколько примерно времени эта задача займет?");
+$name = readLine("Как тебя зовут?");
+$tasks_amount = (int)readLine("Сколько задач Вы запланировали на сегодня? Введите число.");
 
-$task2 = readLine("Какая задача стоит перед вами сегодня?");
-$time2 = (int)readLine("Сколько примерно времени эта задача займет?");
+if(gettype($tasks_amount) !== 0) {
+    echo "Нужно ввести корректное число";
+} else {
 
-$task3 = readLine("Какая задача стоит перед вами сегодня?");
-$time3 = (int)readLine("Сколько примерно времени эта задача займет?");
+    $current_task = '';
+    $current_time = null;
+    $total_time = null;
+    $tasks_array = [];
+    $times_array = [];
 
-$total_time = $time1 + $time2 + $time3;
 
-echo "$name, сегодня у вас запланировано $total_amount приоритетных задачи на день:
-- $task1 ({$time1}ч)
-- $task2 ({$time2}ч)
-- $task3 ({$time3}ч)
-Примерное время выполнения плана = {$total_time}ч";
+    for($i = 0; $i < $tasks_amount; $i++) {
+        $current_task = readLine("Какая задача стоит перед вами сегодня?");
+        $current_time = (int)readLine("Сколько примерно времени эта задача займет?");
+
+        array_push($tasks_array, $current_task);
+        array_push($times_array, $current_time);
+
+        $total_time = array_sum($times_array);
+    }
+
+    echo "$name, сегодня у вас запланировано $tasks_amount приоритетных задачи на день:";
+
+    for($i = 0; $i < count($tasks_array); $i++) {
+            echo "- $tasks_array[$i] ({$times_array[$i]}ч)";
+    }
+
+    echo "Примерное время выполнения плана = {$total_time}ч";
+
+}
+
+
+//Task 3
+$finger = (int)readLine("Введите число и я определю палец: ");
+
+if(($finger - 1) % 8 === 0) {
+    echo "Большой палец";
+} elseif ($finger % 8 === 0 || $finger % 8 === 2) {
+    echo "Указательный палец";
+} elseif (($finger + 1) % 4 === 0) {
+    echo "Средний палец";
+} elseif ($finger % 8 === 4 || $finger % 8 === 6) {
+    echo "Безымянный палец";
+} elseif (($finger - 5) % 8 === 0) {
+    echo "Мизинец  палец";
+};
