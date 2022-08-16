443852bca9c94c874a177cd774e60b78384a6760
Natalya <m_d.r.lecter@protonmail.com>
2022-08-16 23:17:13 +0400

    functions

--- index.php
+++ index.php
@@ -2,93 +2,85 @@
 
 //Task 1
 
-$arr_first = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-$arr_second = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+$array_int =  [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2];
+$even_odd = fn($number):string => $number % 2 === 0? "even" : "odd";
 
-$result_arr = array_map(function ($ind1, $ind2) {
-    return $ind1 * $ind2;
-}, $arr_first, $arr_second);
-
-var_dump($result_arr);
+print_r(array_map($even_odd ,$array_int));
 
 //Task 2
 
-$name = readline("Как тебя зовут?");
-$celebration = readline("Что празднуем?");
+function sort_array(array $arr):array{
+    $max = asort($arr);
+    $min = arsort($arr);
+    $avg = array_sum($arr)/count($arr);
 
-function felicitation ($name, $celebration) {
-    $wishes = ["счастья", "здоровья", "благополучия", "терпения", "воображения", "достатка", "долголетия"];
-    $epithets = ["железного", "огромного", "прекрасного", "полного", "бесконечного", "золотого", "безудержного"];
-
-    $congrats = "";
-    $gaps = [", ", " и ", " "];
-    $selected_wishes = array_rand($wishes, 3);
-    $selected_epithets = array_rand($epithets, 3);
-
-    for($i = 0; $i < count($selected_wishes); $i++) {
-        $congrats .= $epithets[$selected_epithets[$i]] . " " . $wishes[$selected_wishes[$i]] . $gaps[$i];
-    }
-
-    return "Дорогой $name, в этот светлый праздник - $celebration, от всего сердца поздравляю тебя, желаю тебе $congrats!";
+    return ["max" => $max, "min" => $min, "avg" => round($avg)];
+    
 }
 
-echo felicitation($name, $celebration);
+sort_array($array_int);
 
-//Task 3
-$arr_half = [1,2,3,4,5,0,0,0,0,0];
-$double_arr = [];
+// Task 3
 
-for($i = 0, $j = 0; $i < 5; $i++, $j+=2) {
-    $double_arr[$j] = $arr_half[$i];
-    $double_arr[$j+1] = $arr_half[$i];
-}
-
-var_dump($double_arr);
-
-//Task 4
-$ranged_arr = range(1, 200);
-shuffle($ranged_arr);
-$random_arr = array_slice($ranged_arr, 100);
-
-var_dump($random_arr);
-
-//Task 5
-
-$students = [
-    'БАП1320' => [ 
-       "Смирнова Христина" => 3,
-       "Рогозин Даниил" => 2,
-       "Золин Владилен" => 3,
-       "Архаткина Владислава" => 3,
-       "Мещерякова Мария" => 3,
-       "Саврасова Фаина" => 3,
-       "Хромченко Зинаида" => 5,
-       "Протасова Майя" => 3,
+$box = [
+    [
+        0 => 'Тетрадь',
+        1 => 'Книга',
+        2 => 'Настольная игра',
+        3 => [
+            'Настольная игра',
+            'Настольная игра',
+        ],
+        4 => [
+            [
+                'Ноутбук',
+                'Зарядное устройство'
+            ],
+            [
+                'Компьютерная мышь',
+                'Набор проводов',
+                [
+                    'Фотография',
+                    'Картина'
+                ]
+            ],
+            [
+                'Инструкция',
+                [
+                    'Ключ'
+                ]
+            ]
+        ]
     ],
-    "ИТ720" => [
-       "Ябров Тимур" => 4,
-       "Белорусов Ефрем" => 4,
-       "Ягода Назар" => 2,
-       "Ярилова Розалия" => 4,
-       "Нырко Платон" => 2,
-       "Калинин Агап" => 4,
-       "Никифоров Юлиан" => 5
-    ],
-  ];
+    [
+        0 => 'Пакет кошачьего корма',
+        1 => [
+            'Музыкальный плеер',
+            'Книга'
+        ]
+    ]
+ ];
 
-  $avr_mark = ['БАП1320' => 0, "ИТ720" => 0];
-  $expelled_list = [];
+ $staff_name = readline("Что ищем?");
 
-foreach($students as $group => $members) {
-    foreach($members as $member => $mark) {
-        $avr_mark[$group] += $mark;
+ function find_staff(string $staff_name, array $box):bool {
 
-        if($mark < 3) {
-            $expelled_list[] = $member;
+    foreach($box as $staff_in_box) {
+        if(gettype($staff_in_box) === "array") {
+            if (find_staff($staff_name, $staff_in_box)) {
+                return true;
+            }
+        } else {
+            if ($staff_name === $staff_in_box) {
+                return true;
+            }
         }
     }
-    $avr_mark[$group] /= count($members);
-}
+    return false;
+ }
 
-print_r($avr_mark);
-print_r($expelled_list);
+ if(find_staff($staff_name, $box)) {
+    print_r("Нашел '{$staff_name}'");
+ } else {
+    print_r("Не нашлось");
+ }
